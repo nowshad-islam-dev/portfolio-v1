3dddccbe28ca6f1f11582526d30010129ef49d13
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-05-05 00:15:09 +0600

    update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from '../assets/apex_webdev_logo.png';
+import logo from '../assets/portfolio_favicon.png';
 
 import { FaGithub } from 'react-icons/fa6';
 import { FaLinkedin } from 'react-icons/fa6';
@@ -8,8 +8,12 @@ import { FaInstagram } from 'react-icons/fa6';
 const Navbar = () => {
   return (
     <nav className=" mb-20 flex items-center justify-between py-6">
-      <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-1/2" src={logo} alt="logo" />
+      <div className="flex flex-shrink-0 items-center cursor-pointer">
+        <img
+          className="mx-2 w-16 aspect-video rounded-md"
+          src={logo}
+          alt="logo"
+        />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl ">
         <a target="_blank" href="https://github.com/apex-webdev">
