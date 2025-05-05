00333e757b7125fbf2f4b9cc6edf5686c250f8d8
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-05-05 17:20:47 +0600

    update constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,51 +1,50 @@
-import project1 from '../assets/projects/project-1.jpg';
-import project2 from '../assets/projects/project-2.jpg';
+import ide_x_1 from '../assets/projects/ide-x/ide-x-1.png';
+import ide_x_2 from '../assets/projects/ide-x/ide-x-2.png';
+import ide_x_3 from '../assets/projects/ide-x/ide-x-3.png';
+import ide_x_4 from '../assets/projects/ide-x/ide-x-4.png';
+import angkon_1 from '../assets/projects/angkon/angkon-1.png';
+import angkon_2 from '../assets/projects/angkon/angkon-2.png';
 
-import project4 from '../assets/projects/project-4.jpg';
+export const HERO_CONTENT = `I love challenging and creattive tasks where I can grow at a good pace. Proficient in modern technologies like React, Git, Node.js, Redis, Linux and TypeScript, I deliver scalable solutions powered by AWS, Docker, Postgres, GraphQl & MongoDB, ensuring seamless integration from front-end to back-end. With strong communication skills and fluency in English, I thrive in collaborative environments and ensure efficient solution of problems provided to me.`;
 
-export const HERO_CONTENT = `I love challenging and creattive tasks where I can grow at a good pace. Proficient in modern technologies like React, Node.js, and TypeScript, I deliver scalable solutions powered by AWS, Docker, GraphQl & MongoDB, ensuring seamless integration from front-end to back-end. With strong communication skills and fluency in English, I thrive in collaborative environments and ensure efficient solution of problems provided to me.`;
-
-export const ABOUT_TEXT = `I'm a passionate full stack developer with expertise in building dynamic, user-friendly applications. With hands-on experience in React, Tailwind, Bootstrap, Node.js, and TypeScript, I enjoy creating seamless front-end experiences paired with robust back-end systems using Express, MongoDB, GraphQl and Redis. I'm proficient in deploying and managing applications in cloud environments like AWS, and I use Docker and Nginx to ensure scalability and efficient delivery. With a strong focus on performance and code quality, I'm always exploring the latest web development trends to create cutting-edge solutions.`;
+export const ABOUT_TEXT = `I'm a passionate full stack engineer with expertise in building dynamic, user-friendly applications. With hands-on experience in React, Tailwind, Material UI, Node.js, and Javascript, I enjoy creating seamless front-end experiences paired with robust back-end systems using Express, MongoDB, Postgres, GraphQl and Redis. I'm proficient in deploying and managing applications in cloud environments like AWS, and I use Docker, Kafka and Nginx to ensure scalability and efficient delivery. With a strong focus on performance and code quality, I'm always exploring the latest web development trends to create cutting-edge solutions.`;
 
 export const EXPERIENCES = [
   {
-    year: '2021 - 2023',
-    role: ' Full Stack Developer',
-    company: 'SOLshare (solar energy network)',
-    description: `Worked with a team in developing and maintaining web applications using various technologies.`,
-    technologies: ['Typescript', 'AWS', 'Next.js', 'Redis'],
+    year: '2023 - 2024',
+    role: ' Backend Developer',
+    company: 'Brokerage (real estate & housing)',
+    technologies: ['Javascript', 'Express', 'Redis', 'Postgres', 'Docker'],
   },
   {
-    year: '2023 - 2024',
-    role: 'Backend Engineer',
-    company: 'ShareChat(social media platform)',
-    description: `Crafted business logic and data modelling with a pretty large team and maintained them.`,
-    technologies: ['Nginx', 'Typescript', 'Docker', 'mySQL', 'Express'],
+    year: '2022 - 2023',
+    role: 'Full Stack Engineer',
+    company: 'ShareChat (social media platform)',
+    technologies: ['Kafka', 'D3.js', 'Javascript', 'mySQL', 'Apache Arrow'],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: 'E-Commerce Website',
-    image: project1,
+    title: 'IDE-X',
+    images: [ide_x_3, ide_x_1, ide_x_4, ide_x_2],
     description:
-      'A fully functional e-commerce website with features like product listing, shopping cart, product filtering, and user authentication.',
-    technologies: ['Nightwatch.js', 'Gatsby', 'Node.js', 'MongoDB'],
+      'IDE-X is a full-stack web application inspired by platforms like CodePen. It allows users to create, save and share code snippets in real-time. The app provides a seamless coding experience with features like live previews, dark mode, secure authentication, and GitHub OAuth login.',
+    technologies: [
+      'React',
+      'Monaco-Editor',
+      'Express',
+      'Nodemailer',
+      'MongoDB',
+      'Passport.js',
+    ],
   },
   {
-    title: 'Task Management App',
-    image: project2,
+    title: 'ANGKON',
+    images: [angkon_2, angkon_1],
     description:
-      'An application for managing todos and tracking your expenses for a period with advanced graphing output analytics.',
-    technologies: ['React-native', 'CSS3', 'Firebase', 'D3.js'],
-  },
-
-  {
-    title: 'Blogging Platform',
-    image: project4,
-    description:
-      'A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.',
-    technologies: ['HTML5', 'TailwindCss', 'React', 'Express', 'sqLite'],
+      'A dynamic and extensible drawing app built with React, Zustand, and React-Konva. This canvas supports free drawing, shape creation, selection, transformation, color/stroke editing, erasing, and undo/redo — all with a clean grid background and intuitive toolbar.',
+    technologies: ['React', 'Konva', 'Material UI', 'Tailwind CSS'],
   },
 ];
 
